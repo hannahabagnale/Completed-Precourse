214711e41a1192cb67314c74aecc6de506b53915
hannahabagnale <97657382+hannahabagnale@users.noreply.github.com>
2022-01-19 06:25:28 -0800

    L4

--- L4/Homework/homework.js
+++ L4/Homework/homework.js
@@ -10,13 +10,13 @@ const newNum = null ;
 const newBool = null ;
 
 //Solve the following math problem.
-const newSubtract = 10 - null === 5;
+const newSubtract = 10 - 5 === 5;
 
 //Solve the following math problem
-const newMultiply = 10 * null === 40 ;
+const newMultiply = 10 * 4 === 40 ;
 
 //Solve the following math problem:
-const newModulo = 21 % 5 === null ;
+const newModulo = 21 % 5 === 1 ;
 
 
 
@@ -26,113 +26,185 @@ const newModulo = 21 % 5 === null ;
 //Do not change any of the function names.
 
 function returnString(str) {
+  const returnString = str;
+  return returnString;
   //return the string provided: str
 }
 
+returnString(str);
+console.log(returnString);
+
 function add(x, y) {
+ 
+  
+  
   // x and y are numbers
   // add x and y together and return the value
   // code here
+  const sum = x + y;
+  return sum;
 }
 
 function subtract(x, y) {
   // subtract y from x and return the value
   // code here
+  const difference = x - y;
+  return difference;
 }
 
 function multiply(x, y) {
   // multiply x by y and return the value
   // code here
+  const product = x * y;
+  return product;
 }
 
 function divide(x, y) {
   // divide x by y and return the value
   // code here
+  const quotient = x / y;
+  return quotient;
 }
 
 function areEqual(x, y) {
   // return true if x and y are the same
   // otherwise return false
   // code here
+  if (x = y) {
+    return areEqual = true;
+  } else {
+    return areEqual = false;
+  }
+  
 }
 
 function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
   // code here
+  if (str1 = str2) {
+    return areSameLength = true;
+  } else {
+    return areSameLength = false;
+  }
 }
 
 function lessThanNinety(num) {
   // return true if the function argument: num , is less than ninety
   // otherwise return false
   // code here
+if (num < 90) {
+  return lessThanNinety = true;
+} else {
+  return lessThanNinety = false;
+
 }
 
 function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
   // code here
+  if (num > 50) {
+    return greaterThanFifty = true;
+  } else {
+    return greaterThanFifty = false;
+  }
 }
 
 function getRemainder(x, y) {
   // return the remainder from dividing x by y
   // code here
+  const modulo = x % y
+  return modulo;
 }
 
 function isEven(num) {
   // return true if num is even
   // otherwise return false
   // code here
+  if (num % 2 = 0) {
+    return isEven = true;
+  } else {
+    return isEven = false;
+  }
 }
 
 function isOdd(num) {
   // return true if num is odd
   // otherwise return false
   // code here
+  if (num % 2 != 0) {
+    return isOdd = true;
+  } else {
+    
+  }
 }
 
 function square(num) {
   // square num and return the new value
   // hint: NOT square root!
   // code here
+  const squared = num * num;
+  return squared;
 }
 
 function cube(num) {
   // cube num and return the new value
   // code here
+  const cubed = num * num * num;
+  return cubed;
 }
 
 function raiseToPower(num, exponent) {
   // raise num to whatever power is passed in as exponent
   // code here
+  raiseToPower = Math.pow(num, exponent);
+  console.log(raiseToPower);
 }
 
 function roundNumber(num) {
   // round num and return it
   // code here
+  roundNumber = Math.round(num);
+  console.log(roundNumber);
 }
 
 function roundUp(num) {
   // round num up and return it
   // code here
+  roundUp = Math.ceil(num);
+  console.log(roundUp);
+
 }
 
 function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
   // code here
+  const str = !;
+  console.log('hello world'+ str);
+  return addExclamationPoint;
 }
+
+addExclamationPoint(hello world)
 
 function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Avion', 'School' -> 'Avion School'
   // code here
+  const firstName = Avion;
+  const lastName = School;
+  console.log(firstName + lastName);
+  return combineNames;
 }
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
+  const name = Sam;
+  console.log('Hello ' + name+!);
+  return getGreeting;
 }
 
 // The next two questions will have you implement math area formulas. 
@@ -141,11 +213,15 @@ function getGreeting(name) {
 function getRectangleArea(length, width) {
   // return the area of the rectangle by using length and width
   // code here
+  const rectanglearea = length * width;
+  return rectanglearea;
 }
 
 function getTriangleArea(base, height) {
   // return the area of the triangle by using base and height
   // code here
+  const trianglearea = 0.5 * base * height;
+  return trianglearea;
 }
 
 // Do not modify code below this line.
---
+++ L4/Homework/tests/README.md
@@ -0,0 +1,29 @@
+# Homework: Introduction to Javascript, JSI
+
+## Instructions
+---
+1. Feynman Writing Prompts - In a separate text file that you create, write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
+
+	* Variables
+		Answer: Variables act as objects in Code which have value. For example, you have an object or text which you would like to set as a variable in the program with the following Coding:
+					var favouriteFood = 'Samgyeopsal';
+		the program would then be informed that there exists the "favouriteFood" variable, and it would assume its value or output in any part of the Code where it is indicated would be "Samgyeopsal." To put it in other words, setting variables is important in Coding for the program to be much more simplified for as variables are set, its values are automatically linked to these.
+	
+	* Functions (arguments, `return`)
+		Answer: Functions are used in Code to represent a group of statements' outputs 	in the program. Because it 	mainly represents grouped statements' outputs, it leads to much simplified coding as it could be reused in any other parts of the program, but would still perform the grouped statements' specific task. All in all, Functions may be compared to a variable, and its represented statements, its value, but it is collective, unlike variables which are individually coded. 
+				
+		In other cases, functions may have parameters which mean for specifications of the Functions' outputs or its values, known as "Arguments." In many ways, Arguments or the values of Functions may be used in the program. An Argument of a Function used as a Value refers to a Variable containing a specific Value in the Argument of the Function which would then serve as the output of the Function. On the other hand, Arguments used as Variables mean you do not have to specify such Values for these values are made constant in the whole Function.
+
+		Return Statements of a Function are essential as they serve as the "Stop" signal to the Function. Once the Function has accomplished its output, the Return Statement would stop any more operations of the Function.
+
+	* `if` statements
+		Answer: In Code, "If" Statements are used to determine individual, specific outputs for individual, 				specific, or undefined Values of the set Variables. To put it in another way, in these statements
+				is where you specify pre-determined Values of the Variables.
+	
+	* Boolean values (`true`, `false`)
+		Answer: Boolean Values simply go by two values which are "True" and "False." In simple terms, these values 			only determine whether a variable is of "true," or "false."
+
+
+2. Install Node and NPM.  NPM comes packaged with Node. https://nodejs.org/en/download/
+
+3. From the top level of your `Precourse` folder, run `npm test L4/Homework/tests/JSI.test.js` to run the automated tests.  At first all of the tests will be broken.  You will fill out the functions in `homework.js` to make the tests pass.
