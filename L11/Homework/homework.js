82ae715bea28bb2b53d6561306b2b06263240c90
hannahabagnale <97657382+hannahabagnale@users.noreply.github.com>
2022-01-21 12:18:25 -0800

    L11

--- L11/Homework/homework.js
+++ L11/Homework/homework.js
@@ -6,24 +6,28 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
-  if (n === 0) {
+  if (n == 0 || n == 1) {
     return 1;
-  }
-    return n * nFactorial(n-1);
-  
+  } else {
+    return n*nFactorial(n-1);
 }
-console.log(nFactorial(3));
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  function fib(n) {
-    if (n <= 1) return 1;
-      return n
-    }
-    return fib(n - 1) + fib (n - 2);
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < n; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 }
+
 
 // Do not modify code below this line.
 // --------------------------------
